ea46adfb4ce1bee61d643a0d203564c84fd7c2e3
roshanzameerali <76032602+roshanzameerali@users.noreply.github.com>
2020-12-17 14:49:43 +0530

    Update Program.cs

--- SampleApplication/Program.cs
+++ SampleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace SampleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine()
+            Console.WriteLine("Roshan");
         }
     }
 }
